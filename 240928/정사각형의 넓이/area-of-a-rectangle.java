e2795f00ce5dd9977bc0de10b73f937326603949
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-09-28 22:49:54 +0900

    Title: 정사각형의 넓이, Time: 115ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240928/정사각형의 넓이/area-of-a-rectangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n < 5){
+            System.out.println("tiny");
+        }else{
+            System.out.println(n*n);
+        }
+    }
+}
