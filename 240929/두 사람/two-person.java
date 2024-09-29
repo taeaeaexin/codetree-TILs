b7be5a80de659abb4bb34ddad45468d98302abc2
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-09-29 16:20:34 +0900

    Title: 두 사람, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/두 사람/two-person.java
+++ 240929/두 사람/two-person.java
@@ -4,11 +4,11 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a_age = sc.nextInt();
-        char a_sex = sc.nextChar();
+        char a_sex = sc.next().charAt(0);
         int b_age = sc.nextInt();
-        char b_sex = sc.nextChar();
+        char b_sex = sc.next().charAt(0);
 
-        if((a_age >= 19 && a_sex = 'M')||(b_age >= 19 && b_sex = 'M')){
+        if((a_age >= 19 && a_sex == 'M') || (b_age >= 19 && b_sex == 'M')){
             System.out.println(1);
         }else{
             System.out.println(0);
