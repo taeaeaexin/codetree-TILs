c3b55a2ff85bee4c35f2ce64e12e7a794b317aaf
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-09-29 16:47:39 +0900

    Title: 짝수만 출력 2, Time: 3051ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

---
+++ 240929/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a,b;
+
+        b = sc.nextInt();
+        a = sc.nextInt();
+        if(b % 2 == 1){
+            b += 1;
+        }
+
+        while(b >= a){
+            System.out.print(b+" ");
+            b += 2;
+        }
+    }
+}
