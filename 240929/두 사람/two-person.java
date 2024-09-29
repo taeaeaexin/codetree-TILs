07a8f9adc03fa9b7ac3851595678cc0b359c0537
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-09-29 16:15:37 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240929/두 사람/two-person.java
+++ 240929/두 사람/two-person.java
@@ -4,9 +4,9 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a_age = sc.nextInt();
-        char a_sex = sc.next();
+        char a_sex = sc.nextChar();
         int b_age = sc.nextInt();
-        char b_sex = sc.next();
+        char b_sex = sc.nextChar();
 
         if((a_age >= 19 && a_sex = 'M')||(b_age >= 19 && b_sex = 'M')){
             System.out.println(1);
