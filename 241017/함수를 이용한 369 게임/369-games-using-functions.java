4d4801adf847acae7cea120591240f7361a30aa5
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-10-17 22:58:53 +0900

    Title: 함수를 이용한 369 게임, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/함수를 이용한 369 게임/369-games-using-functions.java
+++ 241017/함수를 이용한 369 게임/369-games-using-functions.java
@@ -2,31 +2,30 @@ import java.util.Scanner;
 public class Main {
     public static boolean check(int num){
         while(num > 0){
-            if(num % 10 == 3 || num % 10 == 6 || num % 1 == 9){
+            if(num % 10 == 3 || num % 10 == 6 || num % 10 == 9){
                 return true;
             }
             num /= 10;
-
         }
         return false;
     }
 
-    public static int count(int a, int b){
-        int cnt = 0;
-        for(int i = a ; i <= b ; i++){
-            if(i % 3 == 0 && check(i)){
-                cnt++;
-            }
-        }
-        return cnt;
+    public static boolean count(int n){
+        return (n % 3 == 0) || check(n);
     }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int a,b;
+        int a,b,cnt;
+        cnt = 0;
         a = sc.nextInt();
         b = sc.nextInt();
+        for(int i = a ; i <= b ; i++){
+            if(count(i)){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
 
-        System.out.println(count(a , b));
     }
 }
