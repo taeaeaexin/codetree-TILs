c6c7b8653036c2171a92fdf54581fff927a32342
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-10-19 14:54:25 +0900

    Title: 함수를 이용한 윤년 판별, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 241019/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 241019/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -15,6 +15,8 @@ public class Main {
             }else{
                 return true;
             }
+        }else{
+            return false;
         }
     }
 
@@ -22,6 +24,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
+
         System.out.println(func(y));
     }
 }
