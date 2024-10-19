3c5dd12d0fc7099e92103f96e39f02bf50a8b878
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-10-19 17:02:58 +0900

    Title: 두 수의 거듭제곱, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241019/두 수의 거듭제곱/two-numbers-of-squares.java
+++ 241019/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static int func(int a, int b){
-        int answer;
+        int answer = a;
         for(int i = 0 ; i < b ; i++){
             answer = answer*a;
         }
