2d9e36c0bc6ca812e5af8d6093a5315e433f4ede
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-10-20 16:43:01 +0900

    Title: 사칙연산 함수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241020/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static int func1(int a, int b){
+        return a+b;
+    }
+
+    public static int func2(int a, int b){
+        return a-b;
+    }
+
+    public static int func3(int a, int b){
+        return a/b;
+    }
+
+    public static int func4(int a, int b){
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a, c, result;
+        a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        c = sc.nextInt();
+
+        if(o == '+'){
+            System.out.println(a+" "+o+" "+c+" = "+func1(a, c));
+        }else if(o == '-'){
+            System.out.println(a+" "+o+" "+c+" = "+func2(a, c));
+        }else if(o == '/'){
+            System.out.println(a+" "+o+" "+c+" = "+func3(a, c));
+        }else if(o == '*'){
+            System.out.println(a+" "+o+" "+c+" = "+func4(a, c));
+        }else{
+            System.out.println("false");
+        }
+    }
+}
