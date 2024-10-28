177e81c43a44b1ec6fbe25b105aefc249141bbcc
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-10-28 14:45:25 +0900

    Title: 두 정수에 대한 연산값, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 241028/두 정수에 대한 연산값/operational-values-for-two-integers.java
+++ 241028/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
-public class Main {
-    public static int a;
-    public static int b;
 
-    public static void func(int a, int b) {
-        if(a > b){
-            a += 25;
-            b *= 2;
-        }else{
-            a *= 2;
-            b += 25;
-        }
-        System.out.println(a+" "+b);
+class wrapper {
+    int value;
+
+    public wrapper(int value) {
+        this.value = value;
     }
+}
+
+public class Main {
+    public static void func(wrapper a, wrapper b) {
+        if(a.value > b.value){
+            a.value += 25;
+            b.value *= 2;
+        }else{
+            a.value *= 2;
+            b.value += 25;
+        }
+
+        return;
+    }
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-        b = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        func(a, b);
+        wrapper a_wr = new wrapper(a);
+        wrapper b_wr = new wrapper(b);
 
+        func(a_wr, b_wr);
+
+        a = a_wr.value;
+        b = b_wr.value;
+
+        System.out.print(a+" "+b);
     }
 }
