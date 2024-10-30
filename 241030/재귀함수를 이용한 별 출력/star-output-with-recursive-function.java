55fe99e458c08280a0a69cac5e15545989da68e1
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-10-30 16:01:25 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 178ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241030/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void printstar(int n){
+        if(n == 0){
+            return;
+        }
+
+        printstar(n-1);
+        for(int i = 0 ; i < n ; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        printstar(N);
+    }
+}
