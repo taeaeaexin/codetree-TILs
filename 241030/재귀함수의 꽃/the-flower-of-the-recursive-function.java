b94aa97b89d432ab628f6376349e83a67b93829e
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-10-30 16:07:13 +0900

    Title: 재귀함수의 꽃, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241030/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void func(int n){
+        if(n == 0)
+            return;
+
+        System.out.print(n+" ");
+        func(n-1);
+        System.out.print(n+" ");
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        func(N);
+    }
+}
