1cf9f117cd54a532e8a1e8961eeebb93c5ee64f8
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-11-02 13:28:32 +0900

    Title: 폭탄 해제, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241102/폭탄 해제/bomb-removal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Bomb{
+    String code;
+    char color;
+    int second;
+
+    public Bomb(String code, char color, int second){
+        this.code = code;
+        this.color = color;
+        this.second = second;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 여기에 코드를 작성해주세요.
+        String code = sc.next();
+        char color = sc.next().charAt(0);
+        int second = sc.nextInt();
+
+        Bomb bomb = new Bomb(code, color, second);
+        System.out.println("code : "+bomb.code);
+        System.out.println("color : "+bomb.color);
+        System.out.println("second : "+bomb.second);
+    }
+}
