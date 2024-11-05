4b27bab3711b8b7bba05ae820379fb4bb6516d69
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-11-05 20:00:23 +0900

    Title: 비오는 날, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241105/비오는 날/rainy-day.java
+++ 241105/비오는 날/rainy-day.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Comparator;
 
 class Data{
     String date;
@@ -18,24 +16,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 여기에 코드를 작성해주세요.
+        String min_date = "9999-99-99";
         int n = sc.nextInt();
-        int min = 21100000;
-        int min_flag = 0;
         Data data[] = new Data[n];
+        Data ans = new Data("","","");
 
         for(int i = 0 ; i < n ; i++){
             String date = sc.next();
             String day = sc.next();
             String weather = sc.next();
             data[i] = new Data(date, day, weather);
-            
-            int i_date = Integer.valueOf(date.substring(0,4)+date.substring(5,7)+date.substring(8,10));
-            if(i_date < min && data[i].weather.equals("Rain")){
-                min = i_date;
-                min_flag = i;
+
+            if(weather.equals("Rain") && date.compareTo(min_date) < -1){
+                min_date = date;
+                ans = new Data(date, day, weather);
             }
         }
 
-        System.out.println(data[min_flag].date+" "+data[min_flag].day+" "+data[min_flag].weather);
+        System.out.println(ans.date+" "+ans.day+" "+ans.weather);
     }
 }
