3898725d747fe6c421a68744f328ca5f95c7730e
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-11-10 23:37:07 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 241110/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
+++ 241110/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -13,7 +13,7 @@ public class Main {
         int n = sc.nextInt();
         int arr[] = new int[n];
         arr[0] = 1;
-        arr[1] = 1;
+        if(n != 1) arr[1] = 1;
         for(int i = 2 ; i <= n-1 ; i++){
             arr[i] = arr[i-1] + arr[i-2];
         }
