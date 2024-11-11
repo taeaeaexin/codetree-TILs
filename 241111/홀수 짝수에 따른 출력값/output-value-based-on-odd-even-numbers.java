e1dd575ce3066476d72b6a8759034f6166854323
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-11-11 18:42:38 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241111/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
+++ 241111/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class Main {
     public static int func(int n){
-        if(n == 1 || n == 2)
-            return 0;
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
 
         return (n-1) + n;
     }
