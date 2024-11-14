fa1d13ecd182f9e93c1e27f9207f65b1df1c071c
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-11-14 15:31:11 +0900

    Title: DateTime to DateTime, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241114/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 여기에 코드를 작성해주세요.
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int answer = 0;
+        if(a < 11){
+            answer = -1;
+        }else if(a == 11 && b < 11){
+            answer = -1;
+        }else if(a == 1 && b == 11 && c < 11){
+            answer = 1;
+        }else{
+            System.out.println((a*24*60+b*60+c)-(11*24*60+11*60+11));
+        }
+    }
+}
