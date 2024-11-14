34930996c8d1e3bcd3906c2bead342db61b0e64c
문태신 <68319384+taeaeaexin@users.noreply.github.com>
2024-11-14 15:31:33 +0900

    Title: DateTime to DateTime, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241114/DateTime to DateTime/datetime-to-datetime.java
+++ 241114/DateTime to DateTime/datetime-to-datetime.java
@@ -12,7 +12,7 @@ public class Main {
             answer = -1;
         }else if(a == 11 && b < 11){
             answer = -1;
-        }else if(a == 1 && b == 11 && c < 11){
+        }else if(a == 1 && b == 11 && c <= 11){
             answer = 1;
         }else{
             System.out.println((a*24*60+b*60+c)-(11*24*60+11*60+11));
